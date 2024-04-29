085c0d02c6830b7f5744e2cd8a04819e31197153
Lucas Ferreira <lucas.98sf@gmail.com>
2024-04-29 18:30:47 -0300

    respond 404

--- app/server.go
+++ app/server.go
@@ -4,7 +4,14 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+type HttpHeader struct {
+	method  string
+	path    string
+	version string
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -20,6 +27,26 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	conn.Write([]byte("HTTP/1.1 200 OK \r\n\r\n"))
+	req := make([]byte, 1024)
+	_, err = conn.Read(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	str_req := strings.Split(string(req), " ")
+	header := HttpHeader{method: str_req[0], path: str_req[1], version: str_req[2]}
+
+	var res []byte
+	if header.path == "/" {
+		res = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		res = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
+	_, err = conn.Write(res)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
